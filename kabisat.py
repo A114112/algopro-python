089a99f58bdc1e36d2eb344bb2d6e0ac0cee132c
Hiskia Anggi Puji Pratama <hiskianggi@gmail.com>
2021-03-02 09:37:02 +0700

    Update kabisat.py

--- kabisat.py
+++ kabisat.py
@@ -1,9 +1,15 @@
-year = int(input())
-if year % 400 == 0:
-    print('is_leap_year')
-elif year % 100 == 0:
-    print('not_leap_year')
-elif year % 4 == 0:
-    print('is_leap_year')
-else:
-    print('not_leap_year')
+# Tahun Kabisat
+
+def main():
+  year = int(input())
+  if year % 400 == 0:
+      print('is_leap_year')
+  elif year % 100 == 0:
+      print('not_leap_year')
+  elif year % 4 == 0:
+      print('is_leap_year')
+  else:
+      print('not_leap_year')
+
+if __name__ == '__main__':
+  main()
